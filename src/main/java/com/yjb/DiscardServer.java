de63a52242f774955133a39e2904dd7e51680207
yjb <731012151@qq.com>
2018-12-20 00:17:59 +0800

    提交第一章代码

---
+++ src/main/java/com/yjb/DiscardServer.java
@@ -0,0 +1,40 @@
+package com.yjb;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class DiscardServer {
+    private int port;
+    public DiscardServer(int port){
+        this.port = port;
+    }
+
+    public void run() throws Exception{
+        EventLoopGroup bosseGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup =  new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bosseGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        public void initChannel(SocketChannel ch) throws Exception{
+                            ch.pipeline().addLast(new DiscardServerHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG,128)
+                    .childOption(ChannelOption.SO_KEEPALIVE,true);
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        }finally {
+            workerGroup.shutdownGracefully();
+            bosseGroup.shutdownGracefully();
+        }
+    }
+}
